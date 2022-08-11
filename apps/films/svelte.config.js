7c2d8e7aabb134f6884c74398e3c42b522d02105
Fractal-Tess <vgfractal@gmail.com>
2022-08-11 19:21:48 +0300

    fixed path issue

--- apps/films/svelte.config.js
+++ apps/films/svelte.config.js
@@ -7,7 +7,7 @@ export default {
     sveltePreprocess({
       postcss: true,
       scss: {
-        prependData: '@use "src/variables.scss" as *;'
+        prependData: '@use "src/styles/variables.scss" as *;'
       },
       sourceMap: !!process.env.TAURI_DEBUG
     })
