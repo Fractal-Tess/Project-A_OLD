a067671d505ff9f809c0ef404f423773c99f53d0
Fractal-Tess <vgfractal@gmail.com>
2022-09-02 15:55:15 +0300

    updated transition component

--- packages/ui/components/Transitions/SimpleTransition.svelte
+++ packages/ui/components/Transitions/SimpleTransition.svelte
@@ -4,6 +4,11 @@
   export let transitionDuration = 500;
   let _class = '';
   export { _class as class };
+
+  export let outX = -15;
+  export let inX = 30;
+  export let outY = 0;
+  export let inY = 0;
 </script>
 
 <div class="grid {_class}">
@@ -11,11 +16,12 @@
     <div
       class="row-end-1 col-end-1"
       in:fly={{
-        x: 30,
+        x: inX,
+        y: inY,
         delay: transitionDuration,
         duration: transitionDuration
       }}
-      out:fly={{ x: -15, duration: transitionDuration }}
+      out:fly={{ x: outX, y: outY, duration: transitionDuration }}
     >
       <slot />
     </div>
